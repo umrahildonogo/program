c0d715de447314712b84011773cfdebce41ff6d7
raccoon <donogo.umrahil@yandex.ru>
2023-08-14 19:35:20 +0300

    im genius

--- Program.cs
+++ Program.cs
@@ -12,20 +12,8 @@ int number2 = Convert.ToInt32(Console.ReadLine());
 Console.WriteLine ("Введите третье число");
 int number3 = Convert.ToInt32(Console.ReadLine());
 
-if (number1 > number2 >> number3)
-{
-    Console.WriteLine ("Максимальное число:");
-    Console.WriteLine (number1);
+if (number1 > number2 && number1 > number3) Console.WriteLine(number1);
 
-    if (number2 > number1 >> number3)
-    {
-        Console.WriteLine ("Максимальное число:");
-        Console.WriteLine (number2);
+else if (number2 > number1 && number2 > number3) Console.WriteLine(number2);
 
-        if (number3 > number1 >> number2)
-        {
-            Console.WriteLine ("Максимальное число:");
-            Console.WriteLine (number3);
-         }
-    }
-}
+else if (number3 > number1 && number3 > number2) Console.WriteLine(number3);
